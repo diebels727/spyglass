1e46728d5bba569ac4756c85e8f00086c391b4e6
diebels727 <diebelsalternative@hotmail.com>
2014-08-21 14:25:34 -0400

    First connect.

--- megalog.go
+++ megalog.go
@@ -2,25 +2,34 @@ package megalog
 
 import(
   "github.com/diebels727/tangent"
+  irc "github.com/xales/tangent/irc"
 )
 
 type MegaLog struct {
-  Nick,Username,Server,Channel string
-  Port int
+  User *tangent.User
+  Server,Channel,Port string
   Context *tangent.Context
 }
 
-func New(nick string,username string,server string,port int,channel string) (*MegaLog) {
+func New(nick string,username string,server string,port string,channel string) (*MegaLog) {
   context := tangent.New()
-  return &MegaLog{nick,username,server,channel,port,context}
+  hostmask := irc.HostMask{Nick: nick,Ident: username}
+  user := &tangent.User{HostMask: hostmask, Real: username}
+  return &MegaLog{ User:user,
+                   Server: server,
+                   Channel: channel,
+                   Port: port,
+                   Context: context }
 }
 
+func (m *MegaLog) Connect() {
+  context := m.Context
+  context.Connect(m.Server+":"+m.Port,false,m.User)
 
-
+}
 
 func (m *MegaLog) Run() {
-
-
+  m.Connect()
   //m.Connect()
   // m.Identify()
   // go func() {
--- megalog_test.go
+++ megalog_test.go
@@ -4,8 +4,13 @@ import "testing"
 
 //just a basic sniff test
 func TestInit(t *testing.T) {
-  m := New("nick","username","server",6667,"channel")
+  m := New("nick","username","server","6667","channel")
   if m.Server != "server" {
     t.Error("Expected \"server\" got ",m.Server)
   }
+}
+
+func TestRun(t *testing.T) {
+  m := New("merryNicksMas","sudormrf","irc.freenode.org","6667","#cinch-bots")
+  m.Run()
 }
